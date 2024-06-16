c52395ba2ed009fbc0ac3453ab55c8ac58a4446b
Pawel <pawmag.code@gmail.com>
2024-06-16 12:13:15 +0200

    Add 2nd test - OneRealRoot

--- src/MainTest.java
+++ src/MainTest.java
@@ -12,4 +12,15 @@ class MainTest {
         assertArrayEquals(expected, result, 1e-9);
     }
 
+    @Test
+    void testOneRealRoot() {
+        double[] expected = {1.0};
+        double[] result = Main.solveEquation(1, -2, 1);
+        assertNotNull(result);
+        assertEquals(1, result.length);
+        assertArrayEquals(expected, result, 1e-9);
+    }
+
+
+
 }
