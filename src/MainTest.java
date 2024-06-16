559de2af1e4ac3678b77e924d9a866a4987355a7
Pawel <pawmag.code@gmail.com>
2024-06-16 12:12:33 +0200

    Add 1st test - TwoRealRoots

---
+++ src/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testTwoRealRoots() {
+        double[] expected = {2.0, 1.0};
+        double[] result = Main.solveEquation(1, -3, 2);
+        assertNotNull(result);
+        assertEquals(2, result.length);
+        assertArrayEquals(expected, result, 1e-9);
+    }
+
+}
